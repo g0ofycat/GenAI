ba8cb9845d98f5fb198cd0035c9b73be4f3d0917
g0ofycat <120335059+g0ofycat@users.noreply.github.com>
2025-08-05 14:46:32 -0400

    Delete TransformerArchitecture/src/GenAITesting.py

--- TransformerArchitecture/src/GenAITesting.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-sys.path.append(os.path.join(os.path.dirname(__file__), "classes"))
-
-from classes.class_inference import Inference
-
-print(Inference.Chat("This is the input", 5))
